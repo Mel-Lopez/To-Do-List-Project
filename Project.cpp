1989d9e68091874f88f99b5893b7a7eb16c9f869
Mel-Lopez <melvis.lopez228@gmail.com>
2023-07-14 21:16:28 -0500

    Added selection menu instructions and user input variable and entry

--- Project.cpp
+++ Project.cpp
@@ -8,6 +8,13 @@ int main() {
 	cout << "          To Do List \n";
 	cout << "   Add, View, and Remove Tasks \n";
 	cout << "----------------------------------------\n";
+	cout << endl;
+	cout << endl;
+	cout << "Select (a)dd, (v)iew, (r)emove, or (q)uit" << endl;
+
+	char userInput;
+	cin >> userInput;
+
 
 
 	system("pause");
