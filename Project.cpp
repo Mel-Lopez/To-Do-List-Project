98db0b514217190de9d81ee13252ec3e2c339cce
Mel-Lopez <melvis.lopez228@gmail.com>
2023-07-15 16:40:17 -0500

    Built  skeleton of of how program will work, also added comments

--- Project.cpp
+++ Project.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main() {
-	cout << "          To Do List \n";
+	cout << "          To-Do List \n";
 	cout << "   Add, View, and Remove Tasks \n";
 	cout << "----------------------------------------\n";
 	cout << endl;
@@ -15,7 +15,29 @@ int main() {
 	char userInput;
 	cin >> userInput;
 
+	while (userInput != 'q') {
+		cout << "Select (a)dd, (v)iew, (r)emove, or (q)uit" << endl;
+		cin >> userInput;
 
+		switch (userInput) {
+		case 'a':
+			//adds item to to-do list
+			break;
+		case 'v':
+			//views to-do list
+			break;
+		case 'r':
+			//removes item from to-do list
+			break;
+		default:
+			cout << "That is not a valid input. Press 'a' for add, 'v' for view, 'r' for remove, or 'q' for quit." << endl;
+			break;
+		}
+	}
+
+	if (userInput == 'q') {
+		cout << "Thank you using my To-do list. See you later!" << endl;
+	}
 
 	system("pause");
 	return 0;
